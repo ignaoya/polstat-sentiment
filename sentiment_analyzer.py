d523cbc3412c3601d5530db5cde71ccc6c65a876
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-08-10 10:34:07 +0200

    added function to iterate through database articles and calculate sentiment ratings, function then makes api calls to update each record with the appropriate rating number

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,10 +1,26 @@
 import nltk
+import requests
 from statistics import mean
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 sia = SentimentIntensityAnalyzer()
+API_URL = 'http://localhost:8000/api/articles/'
 
-def get_article_sentiment(article_text: str) -> float:
+
+def get_article_sentiment(article_text: str) -> int:
     tokenized_sentences = nltk.sent_tokenize(article_text)
     scores = [sia.polarity_scores(sentences)["compound"] for sentences in tokenized_sentences]
-    return mean(scores)*100
+    return int(mean(scores)*100)
+
+def rate_unrated_articles():
+    res = requests.get(API_URL)
+    for article in res.json():
+        if article['rating'] == 0:
+            article['rating'] = get_article_sentiment(article['text'])
+            art_id = str(article.pop('id'))
+            requests.put(API_URL + art_id + '/', data=article)
+
+
+if __name__ == '__main__':
+    rate_unrated_articles()
+
